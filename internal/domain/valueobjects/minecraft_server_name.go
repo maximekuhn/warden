c37a74be4ff3163c040ad64d020a12f19ec185b7
Maxime Kuhn <pingouin2032.maxime@gmail.com>
2025-02-02 18:56:35 +0100

    minecraft server name validation

--- internal/domain/valueobjects/minecraft_server_name.go
+++ internal/domain/valueobjects/minecraft_server_name.go
@@ -1,12 +1,34 @@
 package valueobjects
 
+import (
+	"errors"
+	"fmt"
+	"strings"
+)
+
 type MinecraftServerName struct {
 	val string
 }
 
 func NewMinecraftServerName(val string) (MinecraftServerName, error) {
-	// TODO: validation
-	return MinecraftServerName{val: val}, nil
+	const (
+		maxLength = 24
+		minLength = 8
+	)
+
+	name := MinecraftServerName{}
+	trimmedVal := strings.Trim(val, " ")
+	if trimmedVal == "" {
+		return name, errors.New("name can not be empty")
+	}
+	if len(trimmedVal) > maxLength {
+		return name, fmt.Errorf("name can not exceed %d characters", maxLength)
+	}
+	if len(trimmedVal) < minLength {
+		return name, fmt.Errorf("name must be at least %d characters", minLength)
+	}
+	name.val = trimmedVal
+	return name, nil
 }
 
 func (m MinecraftServerName) Value() string {
