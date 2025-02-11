927910aed6c5aa07ef1afa8be979f4c675d58a0d
Maxime Kuhn <pingouin2032.maxime@gmail.com>
2025-02-11 18:22:35 +0100

    issue #8: added missing cascade SQL relations

--- internal/infra/db/sqlite/migrations/00003_create_permissions_tables.sql
+++ internal/infra/db/sqlite/migrations/00003_create_permissions_tables.sql
@@ -1,11 +1,13 @@
 CREATE TABLE user_policy_plan (
     user_id  TEXT PRIMARY KEY,
-    user_plan INTEGER NOT NULL
+    user_plan INTEGER NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES auth(user_id) ON DELETE CASCADE
 );
 
 CREATE TABLE user_role_server (
     user_id TEXT NOT NULL,
     server_id TEXT NOT NULL,
     user_role INTEGER NOT NULL,
-    PRIMARY KEY (user_id, server_id)
+    PRIMARY KEY (user_id, server_id),
+    FOREIGN KEY (user_id) REFERENCES user_policy_plan(user_id) ON DELETE CASCADE
 );
