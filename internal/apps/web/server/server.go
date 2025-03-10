05ed3cc02940815b0aff5fbdeb0bb7241ec94f0f
Maxime Kuhn <pingouin2032.maxime@gmail.com>
2025-03-10 22:39:52 +0100

    logs: replaced handler/listener/middleware field key by "loggerName"

--- internal/apps/web/server/server.go
+++ internal/apps/web/server/server.go
@@ -9,6 +9,7 @@ import (
 	"github.com/maximekuhn/warden/internal/apps/web/middlewares"
 	"github.com/maximekuhn/warden/internal/domain/async"
 	"github.com/maximekuhn/warden/internal/infra/queue"
+	"github.com/maximekuhn/warden/internal/logger"
 )
 
 type Server struct {
@@ -31,9 +32,9 @@ func (s *Server) Start() error {
 	http.Handle("/assets/", http.StripPrefix("/assets/", fs))
 
 	reqIdMiddleware := middlewares.NewRequestIdMiddleware()
-	loggerMiddleware := middlewares.NewLoggerMiddleware(s.logger.With(slog.Bool("LoggerMiddleware", true)))
+	loggerMiddleware := middlewares.NewLoggerMiddleware(s.logger.With(slog.String(logger.LoggerNameFieldName, "LoggerMiddleware")))
 	sessionMiddleware := middlewares.NewSessionMiddleware(
-		s.logger.With(slog.Bool("SessionMiddleware", true)),
+		s.logger.With(slog.String(logger.LoggerNameFieldName, "SessionMiddleware")),
 		*s.app.authService,
 		s.app.uowProvider)
 	adminMiddleware := middlewares.NewAdminMiddleware(s.conf.Admin.Username, s.conf.Admin.HashedPassword)
@@ -43,29 +44,29 @@ func (s *Server) Start() error {
 	chainAdmin := middlewares.Chain(loggerMiddleware, adminMiddleware)
 
 	indexHandler := handlers.NewIndexHandler(
-		s.logger.With(slog.String("handler", "IndexHandler")),
+		s.logger.With(slog.String(logger.LoggerNameFieldName, "IndexHandler")),
 		s.app.getUserPlanQueryHandler)
 	http.Handle("/", chainWithSession.Middleware(indexHandler))
 
 	loginHandler := handlers.NewLoginHandler(
-		s.logger.With(slog.String("handler", "LoginHandler")),
+		s.logger.With(slog.String(logger.LoggerNameFieldName, "LoginHandler")),
 		s.app.authService,
 		s.app.uowProvider)
 	http.Handle("/login", chain.Middleware(loginHandler))
 
 	logoutHandler := handlers.NewLogoutHandler(
-		s.logger.With(slog.String("handler", "LogoutHandler")),
+		s.logger.With(slog.String(logger.LoggerNameFieldName, "LogoutHandler")),
 		s.app.authService,
 		s.app.uowProvider)
 	http.Handle("/logout", chainWithSession.Middleware(logoutHandler))
 
 	signupHandler := handlers.NewSignupHandler(
-		s.logger.With(slog.String("handler", "SignupHandler")),
+		s.logger.With(slog.String(logger.LoggerNameFieldName, "SignupHandler")),
 		s.app.createUserCmdHandler)
 	http.Handle("/signup", chain.Middleware(signupHandler))
 
 	minecraftServersHandler := handlers.NewMinecraftServersHandler(
-		s.logger.With(slog.String("handler", "MinecraftServersHandler")),
+		s.logger.With(slog.String(logger.LoggerNameFieldName, "MinecraftServersHandler")),
 		s.app.permService,
 		s.app.uowProvider,
 		s.app.createMinecraftServerCmdHandler,
@@ -75,7 +76,7 @@ func (s *Server) Start() error {
 	http.Handle("/minecraft-servers", chainWithSession.Middleware(minecraftServersHandler))
 
 	minecraftServerHandler := handlers.NewMinecraftServerHandler(
-		s.logger.With("handler", "MinecraftServerHandler"),
+		s.logger.With(logger.LoggerNameFieldName, "MinecraftServerHandler"),
 		s.app.startMinecraftServerCmdHandler,
 		s.app.uowProvider,
 		s.app.permService,
@@ -84,7 +85,7 @@ func (s *Server) Start() error {
 	http.Handle("/minecraft-servers/{serverId}", chainWithSession.Middleware(minecraftServerHandler))
 
 	minecraftServerStatusHandler := handlers.NewMinecraftServerStatusHandler(
-		s.logger.With(slog.String("handler", "MinecraftServerStatusHandler")),
+		s.logger.With(slog.String(logger.LoggerNameFieldName, "MinecraftServerStatusHandler")),
 		s.app.permService,
 		s.app.getMinecraftServerQueryHandler,
 		s.app.uowProvider,
@@ -92,21 +93,21 @@ func (s *Server) Start() error {
 	http.Handle("/minecraft-servers/{serverId}/status", chainWithSession.Middleware(minecraftServerStatusHandler))
 
 	minecraftServerStopHandler := handlers.NewMinecraftServerStopHandler(
-		s.logger.With("handler", "MinecraftServerStopHandler"),
+		s.logger.With(logger.LoggerNameFieldName, "MinecraftServerStopHandler"),
 		s.app.permService,
 		s.app.uowProvider,
 		s.app.stopMinecraftServerCmdHandler,
 	)
 	http.Handle("/minecraft-servers/{serverId}/stop", chainWithSession.Middleware(minecraftServerStopHandler))
 
-	healthHandler := handlers.NewHealthcheckHandler(s.logger.With(slog.String("handler", "HealtchCheckHandler")))
+	healthHandler := handlers.NewHealthcheckHandler(s.logger.With(slog.String(logger.LoggerNameFieldName, "HealtchCheckHandler")))
 	http.Handle("/healthcheck", chain.Middleware(healthHandler))
 
-	adminHandler := handlers.NewAdminHandler(s.logger.With(slog.String("handler", "AdminHandler")))
+	adminHandler := handlers.NewAdminHandler(s.logger.With(slog.String(logger.LoggerNameFieldName, "AdminHandler")))
 	http.Handle("/admin", chainAdmin.Middleware(adminHandler))
 
 	usersHandler := handlers.NewUsersHandler(
-		s.logger.With(slog.String("handler", "UsersHandler")),
+		s.logger.With(slog.String(logger.LoggerNameFieldName, "UsersHandler")),
 		s.app.getUsersQueryHandler,
 		s.app.updateUserPlanCmdHandler,
 	)
@@ -121,19 +122,19 @@ func (s *Server) Start() error {
 
 func (s *Server) startEventsQueue() {
 	startServerListener := async.NewStartServerEventListener(
-		s.logger.With("listener", "StartServerEventListener"),
+		s.logger.With(logger.LoggerNameFieldName, "StartServerEventListener"),
 		s.app.uowProvider,
 		s.app.containerManagementService,
 		s.app.eventBus,
 		s.app.minecraftServerStatusService,
 	)
 	serverStartedListener := async.NewServerStartedEventListener(
-		s.logger.With("listener", "ServerStartedEventListener"),
+		s.logger.With(logger.LoggerNameFieldName, "ServerStartedEventListener"),
 		s.app.minecraftServerStatusService,
 		s.app.uowProvider,
 	)
 	stopServerListener := async.NewStopServerEventListener(
-		s.logger.With("listener", "StopServerListener"),
+		s.logger.With(logger.LoggerNameFieldName, "StopServerListener"),
 		s.app.uowProvider,
 		s.app.containerManagementService,
 		s.app.minecraftServerStatusService,
